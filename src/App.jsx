037c57f9a9eefd736741673d1fafa5647a4bf0e0
PREME <preme@MacBookAirPREME.local>
2026-06-03 14:56:31 +0700

    feat: เพิ่มหน้าฟอร์มแจ้งซ่อมสำหรับผู้ใช้ และระบบสลับหน้าเว็บ

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,18 @@ function App() {
         <div className="flex gap-6 mx-auto max-w-7xl">
           <h1 className="text-xl font-bold text-blue-400 mr-4">IT Helpdesk</h1>
 
-          <Link to='/' className="px-3 py-1 font-medium transition-colors rounded hover:bg-gray-700">แจ้งปัญหา (ฝั่งผู้ใช้)</Link>
-          <Link to='dashboard' className="px-3 py-1 font-medium transition-colors rounded hover:bg-gray-700">จัดการปัญหา (ฝั่งไอที)</Link>
+          <Link
+            to="/"
+            className="px-3 py-1 font-medium transition-colors rounded hover:bg-gray-700"
+          >
+            แจ้งปัญหา (ฝั่งผู้ใช้)
+          </Link>
+          <Link
+            to="/dashboard"
+            className="px-3 py-1 font-medium transition-colors rounded hover:bg-gray-700"
+          >
+            จัดการปัญหา (ฝั่งไอที)
+          </Link>
         </div>
       </nav>
 
